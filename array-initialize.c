f962309d4f82e5fb9e72ca75e7e56a1c17b0fa30
bsnu002 <bishnunath200@gmail.com>
2023-04-14 19:13:32 +0600

    array initialize

---
+++ array-initialize.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    // int arr[5] = {100, 20, 30, 40, 50};
+    // int arr[] = {100, 20, 30, 40, 50};
+    // int arr[5] = {100};
+    int arr[5] = {0};
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    
+    return 0;
+}
